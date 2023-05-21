6e73993f8e130232a5da41986f26e2424902cbd0
gabe <gabebelcher5@gmail.com>
2023-05-21 13:51:21 +1200

    Add analog temperature sensor
    Add temperature sensor logic

--- sensorLogic.py
+++ sensorLogic.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 import PCF8591 as ADC
 import RPi.GPIO as GPIO
+import os
 import time
+import math
 from datetime import datetime
 
 from connection import iothub_send_msg, init_connection
 
 #IO LOCATION VARIABLES
-REDLED = 17
-GREENLED = 27
-SOUNDSENSOR = 0    # Sound sensor is on ADC channel 0
-PRESSURESENSOR = 1 # Pressure sensor is on ADC channel 1
+REDLED = 17            # Dual LED RED
+GREENLED = 27          # Dual LED GREEN
+SOUNDSENSOR = 0        # Sound sensor is on ADC channel 0
+PRESSURESENSOR = 1     # Pressure sensor is on ADC channel 1
+TEMPERATURESENSOR = 2  # Temperature sensor is on ADC channel 2
+
 
 
 def setup():
@@ -23,6 +27,7 @@ def setup():
 	GPIO.setup(GREENLED, GPIO.OUT)   # Dual LED GREEN
 
 
+
 def sendEvent(eventType, dataType, data):
 	eventTime = datetime.now()
 
@@ -30,9 +35,20 @@ def sendEvent(eventType, dataType, data):
 
 	time.sleep(0.5)
 
+def printTemperature():
+	temperatureValue = ADC.read(TEMPERATURESENSOR)
+	Vr = 5 * float(temperatureValue) / 255
+	Rt = 10000 * Vr / (5 - Vr)
+	temp = 1/(((math.log(Rt / 10000)) / 3950) + (1 / (273.15+25)))
+	temp = temp - 273.15
+	fixedTemp = float("{:.2f}".format(temp/2))
+	print("Temperature: " + str(fixedTemp) + " C")
+
 def sensorloop():
 	count = 0
 	pressureBool = False
+	lastPrintTime = 0
+
 
 	pressureTotalTime = 0.0
 	pressureStartTime = 0.0
@@ -40,6 +56,12 @@ def sensorloop():
 	while True:
 		voiceValue = ADC.read(SOUNDSENSOR)
 		pressureValue = ADC.read(PRESSURESENSOR)
+
+		#only print if 5 seconds have passes
+		if time.time() - lastPrintTime >= 5:
+			lastPrintTime = time.time()
+			printTemperature()
+
 		
 		# Pressure logic:
 		# Will send data if pressure is taken off of sensor (disturbance)
@@ -55,7 +77,7 @@ def sensorloop():
 			elapsed_time = time.time() - pressureStartTime
 			pressureTotalTime += elapsed_time
 
-			sendEvent("pressure", "timeElapsed", pressureTotalTime)
+			#sendEvent("pressure", "timeElapsed", pressureTotalTime)
 
 			pressureBool = False
 			GPIO.output(GREENLED, GPIO.LOW)
@@ -68,7 +90,7 @@ def sensorloop():
 		# Will send data if sound less than threshold is detected
 		if voiceValue < 50:
 			count += 1
-			sendEvent("sound", "count", count)
+			#sendEvent("sound", "count", count)
 
 
 if __name__ == '__main__':
